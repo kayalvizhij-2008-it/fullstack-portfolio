c93e1ef4155a62f42abee81120b7d815a766c345
Kayalvizhi <kayalvizhij2008@gmail.com>
2026-05-16 16:01:05 +0530

    Completed full stack portfolio

---
+++ frontend/script.js
@@ -0,0 +1,33 @@
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const contactData = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value
+  };
+
+  try {
+
+    const response = await fetch("http://localhost:5000/api/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(contactData)
+    });
+
+    const data = await response.json();
+
+    alert(data.message);
+
+    form.reset();
+
+  } catch (error) {
+
+    alert("Something went wrong");
+
+  }
+});
