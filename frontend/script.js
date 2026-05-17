81411ace402a02e50b8d073198c88b669190d6ff
Kayalvizhi <kayalvizhij2008@gmail.com>
2026-05-17 14:31:57 +0530

    connected frontend to render backend

--- frontend/script.js
+++ frontend/script.js
@@ -11,7 +11,7 @@ form.addEventListener("submit", async (e) => {
 
   try {
 
-    const response = await fetch("http://localhost:5000/api/contact", {
+    const response = await fetch("https://fullstack-portfolio-b6b0.onrender.com/api/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
